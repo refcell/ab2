fdd7cb66de41025d2075872849da3a67ce14bcb3
Andreas Bigger <abigger87@gmail.com>
2019-03-24 01:14:48 -0700

    added logos

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
+    response: "",
+    post: "",
+    responseToPost: ""
   };
   componentDidMount() {
     this.callApi()
@@ -13,24 +13,24 @@ class App extends Component {
       .catch(err => console.log(err));
   }
   callApi = async () => {
-    const response = await fetch('/api/hello');
+    const response = await fetch("/api/hello");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
   handleSubmit = async e => {
     e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
+    const response = await fetch("/api/world", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ post: this.state.post }),
+      body: JSON.stringify({ post: this.state.post })
     });
     const body = await response.text();
     this.setState({ responseToPost: body });
   };
-render() {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
